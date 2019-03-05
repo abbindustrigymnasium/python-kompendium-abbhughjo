607a9d1f7f2b7703cf1a333f68e50aa30b1b71b0
abbhughjo <43879922+abbhughjo@users.noreply.github.com>
2019-03-05 15:02:03 +0100

    Create Kapitel 4.py

---
+++ Kapitel 4.py
@@ -0,0 +1,39 @@
+
+#4.1
+
+# tot = range(1000000)
+
+# for nummer in tot:
+#     hej = nummer + nummer
+
+# print(hej)
+
+#4.2
+
+# tot = range(500)
+
+# for num in tot:
+#     if num % 2 == 1:
+#         hej = num + num
+
+# print(hej)
+
+#4.3
+
+# registrerade =["Anna", "Eva", "Erik", "Lars", "Karl"]
+# avanmälningar =["Anna", "Erik", "Karl"]
+
+# for names in avanmälningar:
+#     hej = avanmälningar.index(names)
+#     del registrerade[hej]
+
+# print ( registrerade )
+
+#4.4
+
+# förnamn =["Maria", "Erik", "Karl"]
+# efternamn =[" Svensson ", " Karlsson ", " Andersson "]
+
+# for namn in förnamn:
+#     for last in efternamn:
+#         print(namn + " " + last)
