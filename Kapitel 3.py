fc95231bfabcbd293ed83c9ea72408f186a029f9
abbhughjo <43879922+abbhughjo@users.noreply.github.com>
2019-03-05 14:40:38 +0100

    Create Kapitel 3.py

---
+++ Kapitel 3.py
@@ -0,0 +1,123 @@
+
+#Kapitel 3
+
+#Makera koden du vill använda och klicka på ctrl och * för att av kommentera och klicka sedan på samma sak för att kommentera bort igen för att använda en ny kod 
+
+#3.1
+
+# male = [
+# " Erik ",
+# " Lars ",
+# " Karl ",
+# " Anders ",
+# " Johan "
+# ]
+# female = [
+# " Maria ",
+# " Anna ",
+# " Margareta ",
+# " Elisabeth ",
+# " Eva "
+# ]
+# print(male[0]+ female[2] + female[4] + male[1])
+
+#3.2
+
+# male = [
+# " Erik ",
+# " Lars ",
+# " Karl ",
+# " Anders ",
+# " Johan "
+# ]
+# female = [
+# " Maria ",
+# " Anna ",
+# " Margareta ",
+# " Elisabeth ",
+# " Eva "
+# ]
+
+
+
+# print(male[0]+ female[2] + female[4] + male[1])
+# del male[2]
+# del female[0]
+
+# print(male)
+# print(female)
+
+#3.3
+
+# male = [
+# " Erik ",
+# " Lars ",
+# " Karl ",
+# " Anders ",
+# " Johan "
+# ]
+# female = [
+# " Maria ",
+# " Anna ",
+# " Margareta ",
+# " Elisabeth ",
+# " Eva "
+# ]
+
+
+
+# print(male[0]+ female[2] + female[4] + male[1])
+# del male[2]
+# del female[0]
+
+# male.append('Hugo')
+
+# print(male)
+# print(female)
+
+#3.4
+
+
+# male = [
+# " Erik ",
+# " Lars ",
+# " Karl ",
+# " Anders ",
+# " Johan "
+# ]
+# female = [
+# " Maria ",
+# " Anna ",
+# " Margareta ",
+# " Elisabeth ",
+# " Eva "
+# ]
+
+# male.sort()
+# female.sort()
+# print(male)
+# print(female)
+
+#3.5
+
+
+# people = [
+# "Erik",
+# "Lars",
+# "Karl",
+# "Anders",
+# "Johan",
+# "Maria",
+# "Anna",
+# "Margareta",
+# "Elisabeth",
+# "Eva"
+# ]
+
+# rem = input("Vilken ska bort? ")
+
+# hej = male.index(rem)
+
+# del male[hej]
+
+# print(male)
