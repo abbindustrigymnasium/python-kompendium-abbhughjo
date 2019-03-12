9232e687b72391dbd6841d35f8ee2c424c1015c0
abbhughjo <43879922+abbhughjo@users.noreply.github.com>
2019-03-12 15:24:41 +0100

    Create Kapitel 5.py

---
+++ Kapitel 5.py
@@ -0,0 +1,57 @@
+ #5.1
+
+
+# könn = input('Ange kön: ')
+# hårfärgg = input('Ange Hårfärg: ')
+# ögonfärgg = input('Ange ögonfärg: ')
+
+
+# if könn == 'Man' and hårfärgg == 'Brun' and ögonfärgg == 'Brun':
+#     print('Du är Daniel Radcliffe')
+# elif könn == 'Man' and hårfärgg == 'Röd' and ögonfärgg == 'Blå':
+#     print('Du är Rupert Grint')
+# elif könn == 'Kvinna' and hårfärgg == 'Brun' and ögonfärgg == 'Brun':
+#     print('Du är Emma Watson eller Selena Gomez')
+# else:
+#     print('error')
+    
+
+#5.2
+
+
+# åldrar = [1,2,3,4, 5, 6, 7, 8, 9, 10 ,11, 12, 13, 14, 15, 16, 17]
+
+
+# sömn = [14, 13, 12, 11.5, 11, 11, 10.5, 10, 10, 10, 9.5, 9, 9, 9, 9, 8.5, 8]
+
+
+# namn = input('Ange ditt namn: ')
+
+# age = int(input('Ange din ålder: '))
+
+
+# if age > 17:
+#     x = 8
+# else:    
+#     i = åldrar.index(age)
+
+#     x = sömn.pop(i)
+
+# print('Hej ' + namn + ' du behöver sova ' + str(x) + ' timmar per natt')
+
+
+#5.3
+
+# norden = ['danmark', 'finland', 'island', 'norge', 'sverige']
+# storbrittanien = ['england', 'nordirland', 'skottland', 'wales']
+
+# hej = input('skriv land som norden eller storbrittaninen finns: ')
+
+# h = hej.lower()
+
+# if h in norden:
+#     print(h + ' norden ligger')
+# elif h in storbrittanien:
+#     print(h + ' ligger i storbrittanien')
+# else:
+#     print(h + ' är nej')
